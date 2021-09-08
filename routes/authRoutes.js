d3cd0641a4878d7161ce1aa03e3d6538c2d9c2c3
Alessandro B. Cesta <54996931+Alessandro1918@users.noreply.github.com>
2021-09-08 15:41:06 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,6 @@ const authController = require('../controllers/authController')
 const routes = Router()
 
 routes.route('/')
-    .post(authController.loginUser)       //Search the user in the db and authenticates them, returning their token
+    .post(authController.loginUser)       //Search the user in the db and returns a jtw token used to access authenticated routes
 
 module.exports = routes
